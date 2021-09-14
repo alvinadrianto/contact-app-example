2d362b4bc319fc56e8641d0345924712ae823dd2
rysx-ex <alvin.stream52@gmail.com>
2021-09-14 22:16:19 +0700

    coba ke heroku

--- utils/db.js
+++ utils/db.js
@@ -4,13 +4,3 @@ mongoose.connect('mongodb://127.0.0.1:27017/wpu', {
   useUnifiedTopology: true,
   useCreateIndex: true,
 });
-
-// // Menambah 1 data
-// const contact1 = new Contact({
-//   nama: 'meliodas',
-//   noHP: '123456789087',
-//   email: 'meliodas@gmail.com',
-// });
-
-// // simpan ke collection
-// contact1.save().then((result) => console.log(result));
